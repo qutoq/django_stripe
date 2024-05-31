7a0a44b6be6fc1e53257b4e9795a13a03bc2d2b1
Denis Loshkarev <den-73-73@yandex.ru>
2024-05-31 23:44:06 +0300

    fix

---
+++ app/shop/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+#from .stripy import webhook_stripe
+
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('item/<int:id>/', views.item_view, name='item_view'),
+    path('buy/<int:id>/', views.buy_item, name='buy_item'),
+    path('create/', views.new_order, name='order_create'),
+    path('success/', views.success, name='success'),
+    #path('webhook/', webhook_stripe, name='webhook'),
+]
+
+app_name = 'shop'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
